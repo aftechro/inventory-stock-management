fb94ebf66a4a5452bed93f485621f5fe8351d5bd
AFTECH.RO <38830718+aftechro@users.noreply.github.com>
2024-10-14 10:50:24 +0100

    Update quote.php

--- app/quote.php
+++ app/quote.php
@@ -31,8 +31,8 @@ if (isset($_GET['query'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create a Quote</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="../assets//css/bootstrap5.3.min.css">
+    <script src="../assets/js/jquery-3.6.0.min.js"></script>
 </head>
 <body>
 <div class="container my-5">
